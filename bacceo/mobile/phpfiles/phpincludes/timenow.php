63674a510d2162023139b6cfbaab5afc24e31612
Siddhu <vydyas@gmail.com>
2017-05-29 12:22:02 +0530

    Mobile folder deleted

--- bacceo/mobile/phpfiles/phpincludes/timenow.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-date_default_timezone_set("Asia/Calcutta");
-echo date("d/m/Y");
-?>
--- bacceo/mobile/phpfiles/user_dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-require("phpincludes/config.inc");
-require("phpincludes/phpfunctions.inc");
-dbConnect();
-
-?>
-
